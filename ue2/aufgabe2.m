55ba485a721623465feb9e7f978f5a06b7913719
Robotias <tobiasfischer56@googlemail.com>
2023-06-03 21:24:15 +0200

    add *.asv (autosave files) to .gitignore regarding aufgabe 2: - add filtering in frequency domain - correct overlap_add.m to produce same output as the other approaches

--- ue2/aufgabe2.m
+++ ue2/aufgabe2.m
@@ -4,13 +4,32 @@ x = cos(2*pi*2500+t) + 0.5 * sin(2*pi*5000*t) + 0.25*cos(2*pi*10000*t) + 0.125*s
 
 b = fir1(50, 0.3, 'low');
 
+% filter offline in time domain
 y_conv = conv(x, b);
 
-y = overlap_add(x, b, 60); 
+% filter offline in frequency domain
+X = fft(x);
+B = fft(b, length(x));
+Y = X .* B;
+y_freq_filt = ifft(Y);
 
-subplot(1,2,1)
-plot(y_conv);
-subplot(1,2,2);
-plot(y);
+% filter online
+N = length(b)+1; % this is the minimal length for N
+y = overlap_add(x, b, N); 
+
+subplot(1,3,1)
+plot((1:length(y_conv))*1/Fs, y_conv);
+xlabel 't'
+ylabel 'x filtered offline (time domain)'
+
+subplot(1,3,2)
+plot(t, y_freq_filt)
+xlabel 't'
+ylabel 'x filtered offline (frequency domain)'
+
+subplot(1,3,3);
+plot((1:length(y))*1/Fs, y);
+xlabel 't'
+ylabel 'x filtered online'
 
 
--- ue2/overlap_add.m
+++ ue2/overlap_add.m
@@ -8,7 +8,7 @@ function [y]=overlap_add(x, b, N)
     y = zeros(1, length(x)+M-1);
    
     for block_start = 1:L:length(x)-L
-        x_m = x(block_start:block_start+L);
+        x_m = x(block_start:block_start+L-1);
         x_m(N) = 0;
         X_m = fft(x_m, N);        
         y_m = ifft(X_m .* B, N);
